e4923a2c08071d88575a05f495712197294918c5
Thanh Tung <nguyen.tttung@gmail.com>
2019-12-03 19:51:27 +0100

    Add project files.

---
+++ DigitalAgencyApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using DigitalAgency.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalAgencyApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
